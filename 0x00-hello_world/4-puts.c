396e1fe59199661ba6141818da75237394a7f642
Lyrn-mukami <mbotelyrn@gmail.com>
2022-04-07 09:08:17 -0700

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - prints "\"Programming is like building a multilingual puzz
+ * le"
+ *
+ * Return:Return 0 because its printing
+ **/
+int main(void)
+{
+	char line[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(line);
+
+	return (0);
+}
