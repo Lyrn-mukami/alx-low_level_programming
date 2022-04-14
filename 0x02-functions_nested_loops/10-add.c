4bf01d78de2e79c78d1913f62b67f315ca17888f
Lyrn-mukami <mbotelyrn@gmail.com>
2022-04-13 20:38:24 -0700

    Add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * add - used to print the last digit of a number
- *@void no return expected
-(*
+ *@num1: number to be added to
+ *@num2: number to be added
  * Description:print the product of numbers)?
  * Return: Always 0
  **/
@@ -14,14 +14,5 @@ int add(int num1, int num2)
 	result = 0;
 
 	result = num1 + num2;
-	
 	return (result);
-}
-int main (void)
-{
-	int n;
-
-	n = add(89, 9);
-	printf("%d\n", n);
-	return (0);
 }
