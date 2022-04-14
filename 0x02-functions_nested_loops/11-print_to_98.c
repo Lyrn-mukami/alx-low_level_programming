871e97df996cede7288c16fa8def97b434019294
Lyrn-mukami <mbotelyrn@gmail.com>
2022-04-13 21:25:01 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
  * print_to_98 - used to print the last digit of a number
  *@n:value to be checked
@@ -9,7 +9,7 @@
  **/
 void print_to_98(int n)
 {
-	while(n < 98)
+	while (n < 98)
 	{
 		printf("%i, ", n);
 		n++;
@@ -21,13 +21,4 @@ void print_to_98(int n)
 	}
 	printf("98");
 	putchar('\n');
-}
-int main(void)
-{
-	print_to_98(0);
-	print_to_98(98);
-	print_to_98(111);
-	print_to_98(81);
-	print_to_98(-10);
-	return (0);
 }
