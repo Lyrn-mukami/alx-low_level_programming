9ec4ba855ce62d43f74536e96df5326582394e99
Lyrn-mukami <mbotelyrn@gmail.com>
2022-04-14 01:52:33 -0700

    Advanced correction

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,15 +10,15 @@ int main(void)
 	int a;
 	int b;
 
-	for (a = '0'; a <= '3'; a++)
+	for (a = '0'; a <= '8'; a++)
 	{
-		for (b = '1'; b<='3'; b++)
+		for (b = '1'; b<='9'; b++)
 		{
 			if(a != b && a < b)
 			{
 			putchar (a);
 			putchar (b);
-				if (a =='2' && b == '3')
+				if (a == '8' && b == '9')
 				{
 					break;
 				}
