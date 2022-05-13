7ed8c14bac4c323b1a335f6c3956ef887befe415
Lyrn-mukami <mbotelyrn@gmail.com>
2022-05-13 02:44:16 -0700

    String compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,13 +2,25 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * *_strcmp - copies a string to a prticular point
+ * _strcmp - compares two strings
  * @s1: destination array
  * @s2: source array
  *
  * Return: char dest
  **/
-int *_strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
-	return (strcmp(*s1, *s2));
+	int i = 0;
+	int j = 0;
+	int diff = 0;
+
+	while (s1[i++] && s2[j++])
+	{
+		if (s1[i] != s2[j])
+		{
+			diff = s1[i] - s2[j];
+			break;
+		}
+	}
+	return (diff);
 }
